ffe8b1b3ffc8f03ed95a101df05a0581d87081d8
tz <nxba9b@foxmail.com>
2026-03-07 00:04:10 +0800

    feat:implement 2.9 exercise

--- Work/report.py
+++ Work/report.py
@@ -43,17 +43,40 @@ def read_prices(filename)->list:
                 print('coudld not parse', row)
             name:str = str(row[0])
             dictionary[name] = price
-            
+
     return dictionary
+
+def make_report(prices:dict,portfolio:list)->list:
+    tuples = []
+    labels = ['Name','Shares','Price','Change']
+
+    for dict in portfolio:
+        new_tuple = (dict['name'], int(dict['shares']), float(prices[dict['name']]), float(prices[dict['name']]-dict['price']))
+        print(new_tuple)
+        tuples.append(new_tuple)    
+
+    for i in range(4):
+        print(f'{labels[i]:>12}', end=' ')
+    print('\n')
+    for i in range(4):
+        value = ''
+        print(f'{value:->12}', end=' ')
+    print("\n")
+    for tuple in tuples:
+        print(f'{tuple[0]:>12}', end = ' ')
+        print(f'{tuple[1]:>12}', end = ' ')
+        print(f'{tuple[2]:>12.2f}', end = ' ')
+        print(f'{tuple[3]:>12.2f}', end = '\n')
+    return tuples
 
 if __name__ == '__main__':
     portfolio = read_portfolio('Data/portfolio.csv') #买时的支数量和价格
     prices = read_prices('Data/prices.csv') # 现在每支股票对应的价格
-    gain:float = 0.0
+    # gain:float = 0.0
+    # for dict in portfolio:
+    #     shares = dict['shares']
+    #     name = dict['name']
+    #     gain += (prices[name] - dict['price']) * dict['shares']
 
-    for dict in portfolio:
-        shares = dict['shares']
-        name = dict['name']
-        gain += (prices[name] - dict['price']) * dict['shares']
-
-    print(f'gain = {gain:0.2f}')
+    # print(f'gain = {gain:0.2f}')
+    make_report(prices,portfolio)
