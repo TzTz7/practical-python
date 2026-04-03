bf6be933377713653c4c22e4da6198cd1ec72705
tz <nxba9b@foxmail.com>
2026-04-03 16:07:44 +0800

    feat: implement 5

--- Work/stock.py
+++ Work/stock.py
@@ -1,13 +1,26 @@
 class Stock:
+    __slots__ = ('name','_shares','price')
     def __init__(self,name,shares,price):
         self.name = name
         self.shares = shares
         self.price = price
+        
+    @property
     def cost(self):
         return self.price*self.shares
     
     def sell(self, nshares):
         self.shares -= nshares
+    
+    @property
+    def shares(self):
+        return self._shares
+        
+    @shares.setter
+    def shares(self,value):
+        if not isinstance(value, int):
+            raise TypeError('Except an int')
+        self._shares = value
 
     def __repr__(self):
         return f'Stock({self.name},{self.shares},{self.price})'
@@ -31,17 +44,12 @@ def ChangeStock(Stock):
         super().__init(name,shares,price)
         self.change=change
 
-        
 def main():
     b = Stock('AAPL', 50, 122.34)
     a = Stock('GOOG',100,490.10)
     c = Stock('IBM', 75, 91.75)
     s = MyStock('GOOG', 100, 490.10, 1.25)
-    print(a)
-    print(s)
-    cols = ['name','shares']
-    for col in cols:
-        print(col+'='+str(getattr(a,col)))
+    print(a,a.cost)
     
 if __name__ == '__main__':
     import sys
